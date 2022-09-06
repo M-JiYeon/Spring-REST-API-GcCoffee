a023e7772910735656b86a92a8fd2df793ed0298
M-JiYeon <201802089@O.CNU.AC.KR>
2022-09-06 14:06:45 +0900

    Add delete, update

--- src/main/java/com/example/gccoffee/model/Product.java
+++ src/main/java/com/example/gccoffee/model/Product.java
@@ -1,6 +1,7 @@
 package com.example.gccoffee.model;
 
 import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.UUID;
 
 public class Product {
@@ -17,8 +18,8 @@ public class Product {
         this.productName = productName;
         this.category = category;
         this.price = price;
-        this.createdAt = LocalDateTime.now();
-        this.updatedAt = LocalDateTime.now();
+        this.createdAt = LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS); // 현재 시각의 정밀도 맞추기.
+        this.updatedAt = LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS);
     }
 
     public Product(UUID productId, String productName, Category category, long price, String description, LocalDateTime createdAt, LocalDateTime updatedAt) {
@@ -61,22 +62,22 @@ public class Product {
 
     public void setProductName(String productName) {
         this.productName = productName;
-        this.updatedAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS);
     }
 
     public void setCategory(Category category) {
         this.category = category;
-        this.updatedAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS);
     }
 
     public void setPrice(long price) {
         this.price = price;
-        this.updatedAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS);
     }
 
     public void setDescription(String description) {
         this.description = description;
-        this.updatedAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS);
     }
 
 }
