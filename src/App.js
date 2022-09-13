395eca54e1dc2f1e0f448511d73560b8c2b64a46
M-JiYeon <201802089@O.CNU.AC.KR>
2022-09-13 15:34:54 +0900

    Add btn event

--- src/App.js
+++ src/App.js
@@ -11,6 +11,13 @@ function App() {
     {id: 'uuid-3', productName: '콜롬비아 커피3', category: '커피빈', price: 5000}
   ]);
   const [items, setItems] = useState([]);
+  const handleAddClicked = id => {
+    const product = products.find(v => v.id === id);
+    const found = items.find(v => v.id === id);
+    const updateItems
+      = found ? items.map(v => (v.id === id) ? { ...v, count: v.count + 1 } : v) : [...items, { ...product, count: 1 }];
+    setItems(updateItems);
+  }
   return (
     <div className="container-fluid">
       <div className="row justify-content-center m-4">
@@ -19,7 +26,7 @@ function App() {
       <div className="card">
         <div className="row">
           <div className="col-md-8 mt-4 d-flex flex-column align-items-start p-3 pt-0">
-            <ProductList products={products}/>
+            <ProductList products={products} onAddClick={handleAddClicked}/>
           </div>
           <div className="col-md-4 summary p-4">
             <Summary items={items}/>
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,14 +1,14 @@
 import React from "react";
 import {Product} from "./Product";
 
-export function ProductList({products = []}) {
+export function ProductList({products = [], onAddClick}) {
   return (
     <React.Fragment>
       <h5 className="flex-grow-0"><b>상품 목록</b></h5>
       <ul className="list-group products">
         {products.map(v =>
           <li key={v.id} className="list-group-item d-flex mt-3">
-            <Product productName={v.productName} category={v.category} price={v.price}/>
+            <Product {...v} onAddClick={onAddClick}/>  { /* argument 와 property 가 동일할 때 ...v 로 표현하면 안의 내용이 나열되서 매핑 */}
           </li>
         )}
       </ul>
